4d36b50c3792e4d7e53358678693ba138e9b7c2a
jeandamour241-beep <jeandamour241@gmail.com>
2026-03-07 14:32:00 +0200

    Updated cors

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://ecommerce-users.vercel.app/", "https://ecommerce-admin-rho-three.vercel.app/"],
     credentials: true,
   }),
 );
